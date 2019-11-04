a7aaaa28da20b47a4a579597a2a5fcb3c25aa494
jarrad m <jarrad.middleton@yahoo.com>
2019-11-05 10:54:22 +1100

    First commit

---
+++ src/components/Clock.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { letters } from './letterConfig.js';
+import styled from 'styled-components';
+
+
+//const letters = ["A","b"]
+
+const LetterWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(11, 20px);
+    grid-template-rows: repeat(10, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+`
+
+function Clock() {
+    const illuminatedLetters = [0,1,3 , 4, 22]
+    return (
+        <LetterWrapper>
+        {   
+            letters.map((item, i) => {
+                if (illuminatedLetters.includes(i)) {
+                    return (
+                        <div key={i} value={item} style={{color: "red"}}>
+                            {item} 
+                        </div>
+                    );
+                }
+                else {
+                    return (
+                        <div key={i} value={item} >
+                            {item} 
+                        </div>
+                    );
+                }
+            }
+        )}
+        </LetterWrapper>
+    )
+}
+
+export default Clock;
